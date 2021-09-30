794d3d46e75f52e7e4e8a14f0618625358e69d2b
Théo Potier <48621976+Nitr0z@users.noreply.github.com>
2021-09-30 10:10:52 +0200

    majs remove hs

--- commands/ban.js
+++ commands/ban.js
@@ -1,15 +1,15 @@
 module.exports = {
-    name: 'hskick',
-    description: "This command kicks a member!",
+    name: 'ban',
+    description: "This command ban a member!",
     execute(message, args){
 
         const target = message.mentions.users.first();
         if(target){
             const memberTarget = message.guild.members.cache.get(target.id);
-            memberTarget.kick();
-            message.channel.send("User has been kicked");
+            memberTarget.ban();
+            message.channel.send("User has been ban");
         }else{
-            message.channel.send(`You coudn't kick that member!`);
+            message.channel.send(`You coudn't ban that member!`);
         }
     }
 }
--- commands/kick.js
+++ commands/kick.js
@@ -1,15 +1,15 @@
 module.exports = {
-    name: 'hsban',
-    description: "This command ban a member!",
+    name: 'kick',
+    description: "This command kicks a member!",
     execute(message, args){
 
         const target = message.mentions.users.first();
         if(target){
             const memberTarget = message.guild.members.cache.get(target.id);
-            memberTarget.ban();
-            message.channel.send("User has been ban");
+            memberTarget.kick();
+            message.channel.send("User has been kicked");
         }else{
-            message.channel.send(`You coudn't ban that member!`);
+            message.channel.send(`You coudn't kick that member!`);
         }
     }
 }
